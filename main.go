5d47692b80b7b33a3c2647dee7973e817e26eabe
AleksZieba <zeeb3d@gmail.com>
2025-02-21 17:39:09 +0100

    REPL Functionality added

--- main.go
+++ main.go
@@ -2,12 +2,24 @@ package main
 
 import(
 	"strings"
+	"bufio" 
+	"fmt"
+	"os"
 ) 
 
-func main() {
+func main() { 
+	scanner := bufio.NewScanner(os.Stdin)
+	fmt.Print("Pokedex > ")
+	for {
+		if scanner.Scan() == true { 
+			input := scanner.Text() 
+			strslice := cleanInput(input) 
+			fmt.Print("Your command was: " + strslice[0])
+		} 
+	}
 }
 
 func cleanInput(text string) []string {
 	text = strings.ToLower(text)
 	return strings.Fields(text)
-}
+}  
